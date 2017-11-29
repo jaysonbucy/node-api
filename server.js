4c593fa3859abc831f4c7f6ebf575caf74f82f0d
Jayson H. Bucy <jayson.h.b@gmail.com>
2017-11-29 14:52:44 -0600

    wip

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/tlModel'),
+  bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb:/localhost/Tododb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('.api/routes/tlRoutes');
+routes(app);
 
 app.listen(port);
 
